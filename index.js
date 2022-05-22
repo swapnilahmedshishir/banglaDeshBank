dfd402d0b1b057772d40c3af3927b902f9fd09ec
@swapnilahmedshishir <ahmed66882298@gmail.com>
2022-05-22 21:56:47 +0600

    logine area done

---
+++ index.js
@@ -0,0 +1,6 @@
+let enterLogineBtn = document.getElementById("enter_login_btn");
+
+enterLogineBtn.addEventListener("click",function(){
+    let loginArea = document.getElementById("login_area");
+    loginArea.style.display= "none";
+});
