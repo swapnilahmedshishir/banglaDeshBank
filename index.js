9e47d6b7c0ce3573a8351daf4edc30df19bc9b0b
@swapnilahmedshishir <ahmed66882298@gmail.com>
2022-05-22 23:52:19 +0600

    deposit part end

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+
+// logine envent handler
 let enterLogineBtn = document.getElementById("enter_login_btn");
 
 enterLogineBtn.addEventListener("click",function(){
     let loginArea = document.getElementById("login_area");
     loginArea.style.display= "none";
-});
+    const transAcation = document.getElementById("transaction_area");
+     transAcation.style.display="block";
+})
+
+// deposit event part 
+const depositBtn = document.getElementById("deposit");
+depositBtn.addEventListener('click',function(){
+    let depositAmount = document.getElementById("deposit_amount").value;
+    let depositAmountConvertNumber = parseFloat(depositAmount);
+     let curentAmaunt = document.getElementById("current_deposit").innerText;
+      let currentDepositAmount = parseFloat(curentAmaunt);
+       let total = depositAmountConvertNumber + currentDepositAmount;
+       document.getElementById('current_deposit').innerText = total;
+       document.getElementById("deposit_amount").value = "";
+})
+
