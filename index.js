1f8170f39c377eef08c2c34d8054a8e8cc584a8f
@swapnilahmedshishir <ahmed66882298@gmail.com>
2022-05-23 10:52:26 +0600

    work done

--- index.js
+++ index.js
@@ -9,15 +9,60 @@ enterLogineBtn.addEventListener("click",function(){
      transAcation.style.display="block";
 })
 
+
+
 // deposit event part 
 const depositBtn = document.getElementById("deposit");
 depositBtn.addEventListener('click',function(){
     let depositAmount = document.getElementById("deposit_amount").value;
     let depositAmountConvertNumber = parseFloat(depositAmount);
-     let curentAmaunt = document.getElementById("current_deposit").innerText;
+     
+    let curentAmaunt = document.getElementById("current_deposit").innerText;
       let currentDepositAmount = parseFloat(curentAmaunt);
-       let total = depositAmountConvertNumber + currentDepositAmount;
+      
+      let total = depositAmountConvertNumber + currentDepositAmount;
        document.getElementById('current_deposit').innerText = total;
+      
+    const currentBlance = document.getElementById("current_balnce").innerText;
+     let curentBlanceNumber = parseFloat(currentBlance);
+     let totalBlanceNumber = total + curentBlanceNumber;
+     document.getElementById("current_balnce").innerText = totalBlanceNumber;
+
        document.getElementById("deposit_amount").value = "";
 })
 
+
+ function spanValueUpdate(id){
+    let currentAmaunt = document.getElementById('id').innerText;
+    let curentAmauntNumber = parseFloat(currentAmaunt);
+    let totalAmount = curentAmauntNumber 
+
+ }
+
+
+
+
+
+
+// withdreaw evnet part 
+
+const   withdreawBtn = document.getElementById('withdraw');
+withdreawBtn.addEventListener("click" , function(){
+    const withdraw = document.getElementById("withdraw_amount").value;
+     let withdraw_amount = parseFloat(withdraw);
+    
+     const current_withdraw = document.getElementById("current_withdraw").innerText;
+      let currentWithdrawAmount = parseFloat(current_withdraw);
+
+      let totalWithdraw = withdraw_amount + currentWithdrawAmount;
+       document.getElementById("current_withdraw").innerText = totalWithdraw;
+        
+       const currentBlance = document.getElementById("current_balnce").innerText;
+       let curentBlanceNumber = parseFloat(currentBlance);
+
+       let totalBlanceNumber =  curentBlanceNumber - totalWithdraw ;
+       document.getElementById("current_balnce").innerText = totalBlanceNumber;
+       
+       
+       document.getElementById('withdraw_amount').value = "";
+})
